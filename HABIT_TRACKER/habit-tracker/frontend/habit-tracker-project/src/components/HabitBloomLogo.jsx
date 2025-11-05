53c1548d841a48b94e5a9490d3392a9393d6c43c
Sheetal <sheetalsingh32003@gmail.com>
2025-11-05 22:16:07 +0530

    Added new components and their corresponding CSS files

---
+++ HABIT_TRACKER/habit-tracker/frontend/habit-tracker-project/src/components/HabitBloomLogo.jsx
@@ -0,0 +1,21 @@
+import './HabitBloomLogo.css'; 
+
+function HabitBloomLogo() {
+  return (
+    <div className="habit-bloom-logo">
+      
+      <div className="habit-with-icon"> 
+        <img 
+          src="/butterfly.gif" 
+          alt="HabitBloom Icon" 
+          className="logo-icon" 
+        />
+        <span className="logo-part-habit">Habit</span>
+      </div>
+      
+      <span className="logo-part-bloom">Bloom</span>
+    </div>
+  );
+}
+
+export default HabitBloomLogo;
---
+++ HABIT_TRACKER/habit-tracker/frontend/habit-tracker-project/src/components/SideBar.jsx
@@ -0,0 +1,6 @@
+function SideBar(){
+
+
+}
+
+export default SideBar;
