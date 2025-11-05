d88afcc0380871e2848c5bb452d4bbdf2750c995
Sheetal <sheetalsingh32003@gmail.com>
2025-11-05 20:25:03 +0530

    Updated Habit Tracker project and cleaned deleted files

---
+++ HABIT_TRACKER/habit-tracker/frontend/habit-tracker-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Heading from './components/Heading';
+
+function App() {
+
+  return (
+    <>
+     <Heading />
+     <button>
+      click me
+    </button>
+    </>
+   
+  )
+}
+
+export default App;
---
+++ HABIT_TRACKER/habit-tracker/frontend/habit-tracker-project/src/components/Heading.jsx
@@ -0,0 +1,10 @@
+
+function Heading(){
+   const name = "bloom";
+ if(name)
+    return  <h1>Habit {name}</h1>;
+   return <h1>Habit Tracker</h1>; 
+    
+}
+
+export default Heading;
